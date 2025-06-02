701ced51e0078872449ce8fdb622b1603e31e8f2
Simon Rothman <srothman@mit.edu>
2025-06-02 18:40:20 -0400

    Add more systs to driver

--- PYTHON_UNFOLD/driver.py
+++ PYTHON_UNFOLD/driver.py
@@ -14,7 +14,7 @@ with open("config/datasets.json", 'rb') as f:
 #    HTsum[what] = get_pickled_histogram_sum(tags, xsecs, 'Apr_23_2025', 'EECres4tee', 'nominal', 'nominal', what)
 #    HTinc[what] = get_pickled_histogram('Apr_23_2025', 'Pythia_inclusive', 'EECres4tee', 'nominal', 'nominal', what)
 
-sample = "Pythia_inclusive"
+sample = "Herwig_inclusive"
 
 hists0 = {
     "reco" : {},
@@ -35,16 +35,34 @@ hists1 = {
     "transfer" : {}
 }
 for key in hists0.keys():
-    hists0[key]['nominal'] = get_pickled_histogram("Apr_23_2025", "Pythia_inclusive", "EECres4tee", "nominal", "nominal", key, statN = 2, statK = 0)
-    hists1[key]['nominal'] = get_pickled_histogram("Apr_23_2025", "Pythia_inclusive", "EECres4tee", "nominal", "nominal", key, statN = 2, statK = 1)
+    for wtsyst in ['nominal', 
+                 'scaleUp', 'scaleDown', 
+                   #'isosfUp', 'isosfDown', 
+                   #'idsfUp', 'idsfDown',
+                   #'triggersfUp', 'triggersfDown',
+                 'PUUp', 'PUDown',
+                   #'PDFUp', 'PDFDown',
+                   #'aSUp', 'aSDown',
+                 'PDFaSUp', 'PDFaSDown']:
+        hists0[key][wtsyst] = get_pickled_histogram("Apr_23_2025", sample, "EECres4tee", "nominal", wtsyst, key, statN = 2, statK = 0)
+        hists1[key][wtsyst] = get_pickled_histogram("Apr_23_2025", sample, "EECres4tee", "nominal", wtsyst, key, statN = 2, statK = 1)
+    for objsyst in ['CH_UP', 'CH_DN', 
+                    'JES_UP', 'JES_DN', 
+                    #"JER_UP", "JER_DN", 
+                    #"UNCLUSTERED_UP", 'UNCLUSTERED_DN',
+                    'TRK_EFF']:
+        if objsyst.endswith('_UP'):
+            name = objsyst[:-3]+'Up'
+        elif objsyst.endswith('_DN'):
+            name = objsyst[:-3]+'Down'
+        else:
+            name = objsyst
 
-    for systwt in ['ISR', 'FSR']:
-        hists0[key][systwt] = (
-            get_pickled_histogram("Apr_23_2025", "Pythia_inclusive", "EECres4tee", "nominal", systwt+"Up", key, statN = 2, statK = 0),
-            get_pickled_histogram("Apr_23_2025", "Pythia_inclusive", "EECres4tee", "nominal", systwt+"Down", key, statN = 2, statK = 0)  
-        )
+        hists0[key][name] = get_pickled_histogram("Apr_23_2025", sample, "EECres4tee", objsyst, "nominal", key, statN = 2, statK = 0)
+        hists1[key][name] = get_pickled_histogram("Apr_23_2025", sample, "EECres4tee", objsyst, "nominal", key, statN = 2, statK = 1)
 
-        hists1[key][systwt] = (
-            get_pickled_histogram("Apr_23_2025", "Pythia_inclusive", "EECres4tee", "nominal", systwt+"Up", key, statN = 2, statK = 1),
-            get_pickled_histogram("Apr_23_2025", "Pythia_inclusive", "EECres4tee", "nominal", systwt+"Down", key, statN = 2, statK = 1)  
-        )
+
+import minimizer
+import numpy as np
+import hist
+from importlib import reload
