88be3b70ca869514b37397756e7aca5b8afd42e6
Simon Rothman <srothman@mit.edu>
2025-06-18 14:14:24 -0400

    build Hunf scripts

---
+++ PYTHON_UNFOLD/scripts/build_Hunf.py
@@ -0,0 +1,118 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Run the minimizer for EEC reconstruction')
+parser.add_argument('RecoTag', type=str)
+parser.add_argument('RecoSample', type=str)
+parser.add_argument('--reco_nboot', type=int, default=2000)
+parser.add_argument('--reco_statN', type=int, default=-1)
+parser.add_argument('--reco_statK', type=int, default=-1)
+parser.add_argument('--reco_wtsyst', type=str, default='nominal')
+parser.add_argument('--reco_objsyst', type=str, default='nominal')
+
+parser.add_argument('GenTag', type=str)
+parser.add_argument('GenSample', type=str)
+parser.add_argument('--gen_nboot', type=int, default=2000)
+parser.add_argument('--gen_statN', type=int, default=-1)
+parser.add_argument('--gen_statK', type=int, default=-1)
+
+parser.add_argument('--run2d', action='store_true')
+
+mutually_exclusive = parser.add_mutually_exclusive_group(required=False)
+mutually_exclusive.add_argument('--statonly', action='store_true')
+mutually_exclusive.add_argument('--conditionOne', type=int, default=None)
+mutually_exclusive.add_argument('--conditionRange', nargs=2, type=int, default=None)
+
+args = parser.parse_args()
+
+import os
+import datasets
+import pickle
+import numpy as np
+
+lossname = 'LOSS_%s_%s_%s' % ('2d' if args.run2d else '1d',
+                              args.GenTag, args.GenSample)
+lossname += '_boot%d' % args.gen_nboot
+if args.reco_statN > 0:
+    lossname += '_%dstat%d' % (args.gen_statN, args.gen_statK)
+
+reconame = 'RECO' 
+reconame += '_boot%d' % args.reco_nboot
+if args.reco_statN > 0:
+    reconame += '_%dstat%d' % (args.reco_statN, args.reco_statK)
+
+respath = os.path.join(datasets.basedir, args.RecoTag, args.RecoSample,
+                       'EECres4tee', 'UNFOLDED',
+                       reconame, lossname,
+                       'minimization_result.pkl')
+
+print("Reading minimization result from", respath)
+with open(respath, 'rb') as f:
+    res_tuple = pickle.load(f)
+
+reco = res_tuple[1]
+res = res_tuple[0]
+
+import minimizer
+
+Htemplate = datasets.get_pickled_histogram(args.RecoTag, args.RecoSample,
+                                           'EECres4tee',
+                                           'nominal', 'nominal',
+                                           'reco',
+                                           statN=args.reco_statN,
+                                           statK=args.reco_statK,
+                                           max_nboot=0)
+
+
+x = res.x
+invhess = res.invhess
+import statistics
+
+syststart = reco.shape[0]
+systend = x.shape[0]
+Nsyst = systend - syststart
+if args.statonly:
+    print("Conditioning out all systematics...")
+    x, invhess = statistics.condition(x, invhess, 
+                                      syststart, systend,
+                                      np.zeros(Nsyst))
+    print("\tx shape:", x.shape)
+    print("\tinvhess shape:", invhess.shape)
+elif args.conditionOne is not None:
+    print("Conditioning out systematic at index %d"%(args.conditionOne))
+    x, invhess = statistics.condition(x, invhess, 
+                                      syststart+args.conditionOne, 
+                                      syststart+args.conditionOne + 1,
+                                      np.zeros(1))
+    print("\tx shape:", x.shape)
+    print("\tinvhess shape:", invhess.shape)
+elif args.conditionRange is not None:
+    print("Conditioning out systematics from indices %d to %d"%(args.conditionRange[0], args.conditionRange[1]))
+    x, invhess = statistics.condition(x, invhess, 
+                                      syststart+args.conditionRange[0], 
+                                      syststart+args.conditionRange[1],
+                                      np.zeros(args.conditionRange[1] - args.conditionRange[0]))
+    print("\tx shape:", x.shape)
+    print("\tinvhess shape:", invhess.shape)
+else:
+    print("Not conditioning out any systematics.")
+    print("\tx shape:", x.shape)
+    print("\tinvhess shape:", invhess.shape)
+
+outname = 'Hunf'
+if args.statonly:
+    outname += '_statonly'
+elif args.conditionOne is not None:
+    outname += '_cond%d' % args.conditionOne
+elif args.conditionRange is not None:
+    outname += '_cond%d-%d' % (args.conditionRange[0], args.conditionRange[1])
+outname += '.pkl'
+
+import minimizer
+minimizer.dump_result(res.x, res.invhess, reco, Htemplate, 2000,
+                      os.path.join(datasets.basedir,
+                                   args.RecoTag,
+                                   args.RecoSample,
+                                   'EECres4tee', 
+                                   'UNFOLDED',
+                                   reconame, lossname,
+                                   outname))
---
+++ PYTHON_UNFOLD/scripts/build_all_Hunfs.py
@@ -0,0 +1,99 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Run the minimizer for EEC reconstruction')
+parser.add_argument('RecoTag', type=str)
+parser.add_argument('RecoSample', type=str)
+parser.add_argument('--reco_nboot', type=int, default=2000)
+parser.add_argument('--reco_statN', type=int, default=-1)
+parser.add_argument('--reco_statK', type=int, default=-1)
+parser.add_argument('--reco_wtsyst', type=str, default='nominal')
+parser.add_argument('--reco_objsyst', type=str, default='nominal')
+
+parser.add_argument('GenTag', type=str)
+parser.add_argument('GenSample', type=str)
+parser.add_argument('--gen_nboot', type=int, default=2000)
+parser.add_argument('--gen_statN', type=int, default=-1)
+parser.add_argument('--gen_statK', type=int, default=-1)
+
+parser.add_argument('--run2d', action='store_true')
+
+parser.add_argument('--MCstatstep', type=int, default=100)
+
+args = parser.parse_args()
+
+import os
+import datasets
+import pickle
+import numpy as np
+import sys
+import shlex
+import subprocess
+
+lossname = 'LOSS_%s_%s_%s' % ('2d' if args.run2d else '1d',
+                              args.GenTag, args.GenSample)
+lossname += '_boot%d' % args.gen_nboot
+if args.reco_statN > 0:
+    lossname += '_%dstat%d' % (args.gen_statN, args.gen_statK)
+
+reconame = 'RECO' 
+reconame += '_boot%d' % args.reco_nboot
+if args.reco_statN > 0:
+    reconame += '_%dstat%d' % (args.reco_statN, args.reco_statK)
+
+respath = os.path.join(datasets.basedir, args.RecoTag, args.RecoSample,
+                       'EECres4tee', 'UNFOLDED',
+                       reconame, lossname,
+                       'minimization_result.pkl')
+
+print("Reading minimization result from", respath)
+with open(respath, 'rb') as f:
+    res_tuple = pickle.load(f)
+res = res_tuple[0]
+reco = res_tuple[1]
+Nsyst = res.x.shape[0] - reco.shape[0]
+
+thecommand = f'scripts/build_Hunf.py {args.RecoTag} {args.RecoSample} ' \
+        f'--reco_nboot {args.reco_nboot} ' \
+        f'--reco_statN {args.reco_statN} ' \
+        f'--reco_statK {args.reco_statK} ' \
+        f'--reco_wtsyst {args.reco_wtsyst} ' \
+        f'--reco_objsyst {args.reco_objsyst} ' \
+        f'{args.GenTag} {args.GenSample} ' \
+        f'--gen_nboot {args.gen_nboot} ' \
+        f'--gen_statN {args.gen_statN} ' \
+        f'--gen_statK {args.gen_statK} ' 
+if args.run2d:
+    thecommand += '--run2d '
+
+python = sys.executable
+thecommand = python + ' ' + thecommand
+
+subprocs = []
+print("Running command:", thecommand)
+print("Nominal")
+subprocs.append(subprocess.Popen(shlex.split(thecommand)))
+        #stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL))
+print("Statonly")
+subprocs.append(subprocess.Popen(shlex.split(thecommand + ' --statonly ')))
+        #stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL))
+
+for i in range(args.MCstatstep, args.gen_nboot, args.MCstatstep):
+    print(f"MCstat 0 to {i}")
+    subprocs.append(subprocess.Popen(shlex.split(thecommand + 
+        f' --conditionRange 0 {i}')))
+        #stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL))
+
+for i in range(args.gen_nboot, Nsyst):
+    print(f'syst {i}')
+    subprocs.append(subprocess.Popen(shlex.split(thecommand + 
+        f' --conditionOne {i}')))
+        #stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL))
+
+print("Waiting for completion...")
+for sp in subprocs:
+    sp.wait()
+    if sp.returncode != 0:
+        print("Error in subprocess:", sp)
+        sys.exit(sp.returncode)
+    else:
+        print("Subprocess completed successfully:", sp)
