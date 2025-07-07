08b451db0de0fb7486a032f9b5496526da8fb824
Simon Rothman <srothman@mit.edu>
2025-07-07 12:19:16 -0400

    Workaround hist bug

--- PYTHON_UNFOLD/scripts/build_HTsum_hist.py
+++ PYTHON_UNFOLD/scripts/build_HTsum_hist.py
@@ -53,22 +53,25 @@ for dset, xsec in zip(dsets, xsecs):
         args.Objsyst, args.Wtsyst, args.what,
         args.reweight, args.r123type,
         max_nboot = total_nboot,
+        from_bkp=False,
         silent=args.mute
-    )
+    ) * samplewt
+    print(dset, Hnext[{'bootstrap' : 0}].sum(flow=True))
     if H is None:
-        H = Hnext * samplewt
+        H = Hnext 
     else:
         if H.axes['bootstrap'].size < Hnext.axes['bootstrap'].size:
             Hnext = Hnext[{'bootstrap' : slice(None, H.axes['bootstrap'].size)}]
         elif H.axes['bootstrap'].size > Hnext.axes['bootstrap'].size:
             H = H[{'bootstrap' : slice(None, Hnext.axes['bootstrap'].size)}]
 
-        H += samplewt * Hnext
+        H = H + Hnext
 
     if total_nboot > 0:
         total_nboot = min(total_nboot, Hnext.axes['bootstrap'].size - 1)
     else:
         total_nboot = Hnext.axes['bootstrap'].size - 1
+    print("\tsum so far:", H[{'bootstrap' : 0}].sum(flow=True))
 
 import os
 outfile = '%s_%s_%s'%(args.what, args.Objsyst, args.Wtsyst)
@@ -87,11 +90,12 @@ if args.r123type is not None:
 
 outfile += "_HTSUM.pkl"
 
+print("FINAL SUM", H[{'bootstrap' : 0}].sum(flow=True))
+
 output_path = os.path.join(datasets.basedir, args.Runtag, 
                            args.outputtag, args.Skimmer, 
                            'hists_0to0', args.Objsyst,
                            outfile)
 os.makedirs(os.path.dirname(output_path), exist_ok=True)
-with open(output_path, 'wb') as f:
-    import pickle
-    pickle.dump(H, f)
+import ioutil
+ioutil.wrapped_write_pickle(output_path, H)
